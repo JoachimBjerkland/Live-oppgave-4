06196e29e45b6adda2f4b5ce738bb92e850625a7
Joachim Bjerkland <Bjerkland2011@hotmail.no>
2024-09-12 11:26:38 +0200

    update

--- hono/src/index.ts
+++ hono/src/index.ts
@@ -1,9 +1,68 @@
+import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.use('/api/*', cors())
+app.use(
+  '/api2/*',
+  cors({
+    origin: 'http://example.com',
+    allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests'],
+    allowMethods: ['POST', 'GET', 'OPTIONS'],
+    exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+    maxAge: 600,
+    credentials: true,
+  })
+)
+
+app.get('/', (c) => c.text('GET /'))
+app.post('/', (c) => c.text('POST /'))
+app.put('/', (c) => c.text('PUT /'))
+app.delete('/', (c) => c.text('DELETE /'))
+
+const dadJokes = [
+  {
+    id: 1,
+    title: "Why don't scientists trust atoms?",
+    answer: "Because they make up everything!"
+  },
+  {
+    id: 2,
+    title: "What do you call a fake noodle?",
+    answer: "An impasta!"
+  },
+  {
+    id: 3,
+    title: "Why did the scarecrow win an award?",
+    answer: "He was outstanding in his field!"
+  },
+  {
+    id: 4,
+    title: "How do you organize a space party?",
+    answer: "You planet!"
+  },
+  {
+    id: 5,
+    title: "What do you call a bear with no teeth?",
+    answer: "A gummy bear!"
+  }
+];
+
+
+app.all('/api/abc', (c) => {
+  return c.json({ success: true })
+})
+app.all('/api2/abc', (c) => {
+  return c.json({ success: true })
 })
 
-export default app
+const port = 3000
+console.log(`Server is running on port ${port}`)
+
+serve({
+  fetch: app.fetch,
+  port
+})
